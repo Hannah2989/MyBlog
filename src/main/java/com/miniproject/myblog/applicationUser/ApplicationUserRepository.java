7bc78b36e20d4ac8e6232dad0be4d6c95882eb5f
Hannah2989 <hana.rostami2020@gmail.com>
2026-07-20 22:01:00 +0330

    ApplicationUser repository created, Blog repository updated

---
+++ src/main/java/com/miniproject/myblog/applicationUser/ApplicationUserRepository.java
@@ -0,0 +1,9 @@
+package com.miniproject.myblog.applicationUser;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ApplicationUserRepository extends JpaRepository<ApplicationUser, Integer> {
+    ApplicationUser findByUsernameAndPassword(String username, String password);
+}
