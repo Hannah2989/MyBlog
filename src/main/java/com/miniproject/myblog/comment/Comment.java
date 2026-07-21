f51d8420e2e8bd6aa8bbf0f72b19d7512214c90b
Hannah2989 <hana.rostami2020@gmail.com>
2026-07-21 21:20:04 +0330

    Comment table created

---
+++ src/main/java/com/miniproject/myblog/comment/Comment.java
@@ -0,0 +1,70 @@
+package com.miniproject.myblog.comment;
+
+import com.miniproject.myblog.blog.Blog;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int id;
+    private String description;
+    private String username;
+    private Boolean approved;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @ManyToOne
+    private Blog blog;
+
+    public Boolean getApproved() {
+        return approved;
+    }
+
+    public void setApproved(Boolean approved) {
+        this.approved = approved;
+    }
+
+    public Blog getBlog() {
+        return blog;
+    }
+
+    public void setBlog(Blog blog) {
+        this.blog = blog;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
