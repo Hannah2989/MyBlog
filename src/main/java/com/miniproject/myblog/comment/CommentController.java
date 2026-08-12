c86e7a7ea27b29bdb1b20501d91556d414e756e3
Hannah2989 <hana.rostami2020@gmail.com>
2026-08-12 15:01:29 +0330

    Comment Controller updated

--- src/main/java/com/miniproject/myblog/comment/CommentController.java
+++ src/main/java/com/miniproject/myblog/comment/CommentController.java
@@ -1,50 +1,37 @@
 package com.miniproject.myblog.comment;
 
-import com.miniproject.myblog.blog.Blog;
-import com.miniproject.myblog.blog.BlogService;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+
+@RestController
 @RequestMapping("/comments")
 public class CommentController {
 
     private final CommentService commentService;
-    private final BlogService blogService;
 
-    public CommentController(CommentService commentService,
-                             BlogService blogService) {
+    public CommentController(CommentService commentService) {
         this.commentService = commentService;
-        this.blogService = blogService;
     }
 
-    @PostMapping("/blogs/{blogId}")
-    public String createComment(@PathVariable int blogId,
-                                @ModelAttribute Comment comment) {
-
-        Blog blog = blogService.findById(blogId);
-
-        comment.setBlog(blog);
-
-        commentService.createComment(comment);
-
-        return "redirect:/blogs/" + blogId;
+    @GetMapping
+    public List<Comment> findAll() {
+        return commentService.findAll();
     }
 
-    @GetMapping("/approve/{id}")
-    public String approveComment(@PathVariable int id) {
-
-        commentService.approveComment(id);
-
-        return "redirect:/blogs";
+    @GetMapping("/{blogId}")
+    public List<Comment> findByBlogId(@PathVariable int blogId) {
+        return commentService.findCommentByBlog(blogId);
     }
 
-    @GetMapping("/delete/{id}")
-    public String deleteComment(@PathVariable int id) {
+    @PostMapping
+    public void save(Comment comment) {
+        commentService.saveComment(comment);
+    }
 
-        commentService.deleteComment(id);
-
-        return "redirect:/blogs";
+    @GetMapping("/approved/{commentId}")
+    public void approvedComment(@PathVariable int commentId) {
+        commentService.approvedComment(commentId);
     }
 
 }
